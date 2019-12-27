937e2071859c6d902e9ac30adf27501797692708
Tim Paine <t.paine154@gmail.com>
2019-12-27 14:32:11 -0500

    working on #2

--- setup.py
+++ setup.py
@@ -1,28 +1,40 @@
 from setuptools import setup, find_packages
 from codecs import open
-from os import path
+import os.path
+import io
 
-here = path.abspath(path.dirname(__file__))
+pjoin = os.path.join
+here = os.path.abspath(os.path.dirname(__file__))
+name = 'greeks'
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+
+def get_version(file, name='__version__'):
+    path = os.path.realpath(file)
+    version_ns = {}
+    with io.open(path, encoding="utf8") as f:
+        exec(f.read(), {}, version_ns)
+    return version_ns[name]
+
+version = get_version(pjoin(here, name, '_version.py'))
+
+with open(pjoin(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open(pjoin(here, 'requirements.txt'), encoding='utf-8') as f:
     requires = f.read().split()
 
 
 setup(
-    name='greeks',
-    version='0.0.2',
+    name=name,
+    version=version,
     description='Analytics library',
     long_description=long_description,
-    url='https://github.com/timkpaine/greeks',
-    download_url='https://github.com/timkpaine/greeks/archive/v0.0.2.tar.gz',
+    url='https://github.com/timkpaine/{name}'.format(name=name),
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='Apache 2.0',
     install_requires=requires,
-    extras_require={'dev': requires + ['pytest', 'pytest-cov', 'pylint', 'flake8', 'codecov', 'mock', 'autopep8', 'bumpversion']},
+    extras_require={'dev': requires + ['pytest', 'pytest-cov', 'flake8', 'codecov', 'mock', 'autopep8', 'bumpversion']},
 
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -32,7 +44,7 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
 
-    keywords='analytics tools',
+    keywords='analytics tools plotting',
 
     packages=find_packages(exclude=['tests', ]),
     package_data={},
